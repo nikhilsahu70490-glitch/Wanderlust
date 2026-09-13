7a104917e04df97414749d6d59a0f80ee4b5864b
Nikhil Sahu <nikhilsahu70490@gmail.com>
2026-09-13 12:52:57 +0530

    Update MongoDB connection URL to use environment variable

--- wanderlust/app.js
+++ wanderlust/app.js
@@ -6,7 +6,8 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+//const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
   .then(() => {
